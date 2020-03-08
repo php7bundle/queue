461c2b5fce88f834b59d82449353c0ec7138feb1
Vitaliy <theyamshikov@yandex.ru>
2020-03-08 20:20:07 +0600

    fix

--- src/Domain/Interfaces/JobInterface.php
+++ src/Domain/Interfaces/JobInterface.php
@@ -2,13 +2,9 @@
 
 namespace PhpBundle\Queue\Domain\Interfaces;
 
-use Psr\Container\ContainerInterface;
-
 interface JobInterface
 {
 
     public function run();
-
-    public function setContainer(ContainerInterface $container = null);
 
 }
